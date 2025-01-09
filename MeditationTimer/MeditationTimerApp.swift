78620b40684527c8bfa0072dd237aeb97b824966
Xingchen Chen <92684741+esemsc-xc1924@users.noreply.github.com>
2025-01-09 14:17:35 +0000

    Initial Commit

---
+++ MeditationTimer/MeditationTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationTimerApp.swift
+//  MeditationTimer
+//
+//  Created by Xingchen Chen on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct MeditationTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeditationTimerTests/MeditationTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationTimerTests.swift
+//  MeditationTimerTests
+//
+//  Created by Xingchen Chen on 09/01/2025.
+//
+
+import Testing
+@testable import MeditationTimer
+
+struct MeditationTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
